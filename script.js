8182e3e5feb5ee1acec252cc97c70c38b3b957eb
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2022-03-07 19:52:32 -0500

    Removed button style to dropdown icons (to-do.txt, dark-mode.css) Finishing script for dropdown display (script.js)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelector("button.dropdown-icon").addEventListener("click", event =
 function showDropdown(event)
 {
   let target = event.target;
-  Array.from(target.parentNode.childNodes).filter(i=>i.nodeType === 1)
-  console.log(Array.from(target.parentNode.childNodes).filter(i=>i.nodeType === 1))
+  console.log("",Array.from(target.parentNode.childNodes).filter(i=>i.nodeType === 1).filter(i=>{return Array.from(i.classList).includes('dropdown-container--user-dropdown')})[0])
+  Array.from(target.parentNode.childNodes).filter(i=>i.nodeType === 1).filter(i=>{return Array.from(i.classList).includes('dropdown-container--user-dropdown')})[0].style.setProperty("display", "block");;//remember to give return value to arrow functions
   //.dropdown-container--user-dropdown
 }
